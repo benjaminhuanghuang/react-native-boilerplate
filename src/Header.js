22644544fff4dc5dbfedb7a3367d68374b688ba8
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-08-02 17:44:47 -0700

    fix

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {View, TextInput } from 'react-native';
+import {StyleSheet, View, TextInput } from 'react-native';
 
 //
 export default class Header extends Component {
